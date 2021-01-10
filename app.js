4bbad0f6b924b3665b91e6d4223d2e1a2a3832dd
Matt <matthew.wilkes90@gmail.com>
2021-01-10 21:23:59 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//jshint esversion:6
+
+const express = require ("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req, res) {
+  console.log(__dirname)
+res.sendFile(__dirname + "/index.html");
+
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('Process terminated')
+  });
+});
