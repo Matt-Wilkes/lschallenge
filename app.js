c4a90b6d417cdb2363910ef49228857d98d3f6b2
Matt <matthew.wilkes90@gmail.com>
2021-01-24 19:20:07 +0000

    before removing header grid

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const express = require ("express");
 const https = require("https");
 const bodyParser = require("body-parser");
 
+
 const app = express();
 
 
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
-  console.log(__dirname)
 res.sendFile(__dirname + "/index.html");
 
 });
