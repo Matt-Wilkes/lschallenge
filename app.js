13187b11facbe4996ca186bc4bfe6ff2cf10d1fc
Matt <matthew.wilkes90@gmail.com>
2021-02-12 14:45:53 +0000

    added responsive nav bar

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-//jshint esversion:6
+// //jshint esversion:6
+//
+// const express = require ("express");
+// const https = require("https");
+// const bodyParser = require("body-parser");
+// const app = express();
+//
+//
+//
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static("public"));
+//
+// app.get("/", function(req, res) {
+// res.sendFile(__dirname + "/index.html");
+//
+// });
+//
+// app.post("/", function(req, res){
+//
+// })
+//
+//
+// app.listen(process.env.PORT || 3000, function() {
+//   console.log("Server started on port 3000");
+// });
+//
+// process.on('SIGTERM', () => {
+//   server.close(() => {
+//     console.log('Process terminated');
+//   });
+// });
 
-const express = require ("express");
-const https = require("https");
-const bodyParser = require("body-parser");
+const navToggle = document.querySelector('#navToggle');
+const nav = document.querySelector('nav');
+const navIcon = document.querySelectorAll('.navIcon');
+const hamburger = document.querySelector('#hamburger');
 
-
-const app = express();
-
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-app.get("/", function(req, res) {
-res.sendFile(__dirname + "/index.html");
-
-});
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
-});
-
-process.on('SIGTERM', () => {
-  server.close(() => {
-    console.log('Process terminated')
+navToggle.addEventListener("click", () => {
+  nav.classList.toggle('open');
+  navIcon.forEach(icon => {
+    icon.classList.toggle('hidden');
   });
+
+});
+
+window.addEventListener("resize", () => {
+  if(document.body.clientWidth > 720) {
+    nav.classList.remove('open');
+    navIcon.forEach(icon => {
+      icon.classList.add('hidden')
+    });
+    hamburger.classList.remove('hidden')
+  }
 });
